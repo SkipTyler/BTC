a0045657ebd97bc211d45feebf27a5f0baf21558
SkipTyler <DetyaJah@protonmail.com>
2018-08-28 22:46:07 +0300

    Merge branch 'gats' of github.com:SkipTyler/BTC
    # Conflicts:
    #	src/scripts/app.js
    #	src/styles/app.scss
    #	src/styles/default.scss

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,8 +5,50 @@ const _preloader = document.querySelector('.js-preloader');
 setTimeout(() => {
 	_preloader.classList.remove('active');
 	document.documentElement.classList.remove('no-scroll');
-}, 3500);
+}, 5150);
 
+// preloader
+(() => {
+	var paper = Raphael("canvas", '100%', '100%');
+	var h = $(window).height();
+	var w = $(window).width();
+	var cDiam = 40;
+	var cx = w / 2;
+	var cy = h / 2;
+
+	var steps = w / cDiam;
+	var incRad = w / steps;
+
+	function drawCircles(){
+		for (var i = 0; i < steps; i++) {
+			 var startDiam = i*steps;
+			var animCircle = Raphael.animation({'stroke-width':steps+1,'r':startDiam+20},1500, "<>").delay(i*50);
+			paper.circle(cx, cy, startDiam).attr({
+				'stroke': 'rgba(250, 148, 121, 1)',
+				'stroke-width': '0',
+				'fill': 'none'
+			}).animate(animCircle);
+		}
+	}
+	var toCenter = paper.path("M" + 0 + "," + h + "L" + 0 + "," + h);
+	toCenter.attr({
+		'stroke': '#fa9479',
+		'stroke-width': '1',
+		'stroke-linejoin': 'round',
+		'fill': 'none'
+	});
+	var transformedPath = Raphael.transformPath("M" + 0 + "," + h + "L" + cx + "," + cy);
+	toCenter.animate({
+		path: transformedPath
+	}, 1000, lineReverse);
+
+	function lineReverse() {
+		var reversedPath = Raphael.transformPath("M" + cx + "," + cy + "L" + cx + "," + cy);
+		toCenter.animate({
+			path: reversedPath
+		}, 1000, ">", drawCircles);
+	}
+})();
 
 
 // delegate events
@@ -125,109 +167,6 @@ document.addEventListener('DOMContentLoaded', function () {
 	})
 })();
 
-
-(() => {
-
-	var colors = new Array(
-		[62,35,255],
-		[60,255,60],
-		[255,35,98],
-		[45,175,230],
-		[255,0,255],
-		[255,128,0]);
-
-	var step = 0;
-//color table indices for:
-// current color left
-// next color left
-// current color right
-// next color right
-	var colorIndices = [0,1,2,3];
-
-//transition speed
-	var gradientSpeed = 0.002;
-
-	function updateGradient()
-	{
-
-		if ( $===undefined ) return;
-
-		var c0_0 = colors[colorIndices[0]];
-		var c0_1 = colors[colorIndices[1]];
-		var c1_0 = colors[colorIndices[2]];
-		var c1_1 = colors[colorIndices[3]];
-
-		var istep = 1 - step;
-		var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-		var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-		var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-		var color1 = "rgb("+r1+","+g1+","+b1+")";
-
-		var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-		var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-		var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-		var color2 = "rgb("+r2+","+g2+","+b2+")";
-
-
-		// if ($(window).width() <= 1024) {
-		// 	$('.roadmap__line').css({
-		// 		background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+")) center/2px 100% no-repeat"}).css({
-		// 		background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%) center/2px 100% no-repeat"});
-		// } else if ($(window).width() <= 680) {
-		// 	$('.roadmap__line').css({
-		// 		background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+")) left/2px 100% no-repeat"}).css({
-		// 		background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%) left/2px 100%  no-repeat"});
-		// } else {
-		// 	$('.roadmap__line').css({
-		// 		background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+")) center/100% 2px no-repeat"}).css({
-		// 		background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%) center/100% 2px no-repeat"});
-		// }
-
-
-		if ($(window).width() >= 680 && $(window).width() < 1024) {
-			$('.roadmap__line').css({
-				background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+")) center/2px 100% no-repeat"}).css({
-				background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%) center/2px 100% no-repeat"});
-		} else if ($(window).width() >= 1024) {
-			$('.roadmap__line').css({
-				background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+")) center/100% 2px no-repeat"}).css({
-				background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%) center/100% 2px no-repeat"});
-		} else {
-			$('.roadmap__line').css({
-				background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+")) left/2px 100% no-repeat"}).css({
-				background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%) left/2px 100%  no-repeat"});
-		}
-
-
-
-		$('.roadmap__line-circle').css({
-			background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-			background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-
-		$('.roadmap__line .after').css({
-			background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-			background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+")"});
-		$('.roadmap__line .before').css({
-			background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-			background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+")"});
-
-		step += gradientSpeed;
-		if ( step >= 1 )
-		{
-			step %= 1;
-			colorIndices[0] = colorIndices[1];
-			colorIndices[2] = colorIndices[3];
-
-			//pick two new target color indices
-			//do not pick the same as the current one
-			colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-			colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-
-		}
-	}
-
-	setInterval(updateGradient,10);
-})();
 
 (() => {
 	const _graph = document.querySelector('.graph__img');
