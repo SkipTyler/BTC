e329e785f0ab2a6777046088e3b966c0d527671d
SkipTyler <DetyaJah@protonmail.com>
2018-09-10 23:40:23 +0300

    Merge branch 'gats' of github.com:SkipTyler/BTC
    # Conflicts:
    #	src/scripts/app.js
    #	src/styles/app.scss
    #	src/styles/default.scss

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -58,37 +58,37 @@ const delegate = (criteria, listener) => {
 })();
 
 //timer
-(() =>{
-	const timer = (parent, data) => {
-		const second = 1000;
-		const minute = second * 60;
-		const hour = minute * 60;
-		const day = hour * 24;
-		const month = day * 30;
-
-		const countDown = new Date(data).getTime();
-
-		const x = setInterval(() => {
-			const now = new Date().getTime();
-			const distance = countDown - now;
-
-			const _parent = document.querySelector(parent);
-			const _month = _parent.querySelector('.js-timerMonth');
-			const _day = _parent.querySelector('.js-timerDay');
-			const _hour = _parent.querySelector('.js-timerHour');
-			const _minutes = _parent.querySelector('.js-timerMinutes');
-			const _seconds = _parent.querySelector('.js-timerSeconds');
-
-			_month.innerHTML = Math.floor((distance / month));
-			_day.innerHTML = Math.floor((distance % month) / day);
-			_hour.innerHTML = Math.floor((distance % day) / hour);
-			_minutes.innerHTML = Math.floor((distance % hour) / minute);
-			_seconds.innerHTML = Math.floor((distance % minute) / second);
-			if (distance < 0) clearInterval(x);
-		}, second);
-	};
-	timer('.js-timer', 'October 4, 2018 23:59:99');
-})();
+// (() =>{
+// 	const timer = (parent, data) => {
+// 		const second = 1000;
+// 		const minute = second * 60;
+// 		const hour = minute * 60;
+// 		const day = hour * 24;
+// 		const month = day * 30;
+//
+// 		const countDown = new Date(data).getTime();
+//
+// 		const x = setInterval(() => {
+// 			const now = new Date().getTime();
+// 			const distance = countDown - now;
+//
+// 			const _parent = document.querySelector(parent);
+// 			const _month = _parent.querySelector('.js-timerMonth');
+// 			const _day = _parent.querySelector('.js-timerDay');
+// 			const _hour = _parent.querySelector('.js-timerHour');
+// 			const _minutes = _parent.querySelector('.js-timerMinutes');
+// 			const _seconds = _parent.querySelector('.js-timerSeconds');
+//
+// 			_month.innerHTML = Math.floor((distance / month));
+// 			_day.innerHTML = Math.floor((distance % month) / day);
+// 			_hour.innerHTML = Math.floor((distance % day) / hour);
+// 			_minutes.innerHTML = Math.floor((distance % hour) / minute);
+// 			_seconds.innerHTML = Math.floor((distance % minute) / second);
+// 			if (distance < 0) clearInterval(x);
+// 		}, second);
+// 	};
+// 	timer('.js-timer', 'October 4, 2018 23:59:99');
+// })();
 
 document.addEventListener('DOMContentLoaded', function () {
     const circles = document.querySelectorAll('.diagrams_image');
